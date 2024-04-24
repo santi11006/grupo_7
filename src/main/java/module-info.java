7c904e7a3bbe55d18e6f1c700f5c2355a32c4b4e
santi <santiagofuentesespinosa@gmail.com>
2024-04-24 19:52:50 +0200

    1.0

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module org.example.proyecto_grupo7 {
     requires javafx.fxml;
     requires java.sql;
     requires org.xerial.sqlitejdbc;
+    requires java.desktop;
 
 
     opens org.example.proyecto_grupo7 to javafx.fxml;
---
+++ src/main/java/org/example/proyecto_grupo7/LoginController.java
@@ -0,0 +1,38 @@
+package org.example.proyecto_grupo7;
+
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+
+public class LoginController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    private Label errorCuentaErronea;
+    @FXML
+    private TextField barraLogin, cajaPassword;
+    @FXML
+    private Button botonEntrar;
+
+    @FXML
+    public void onClick(ActionEvent actionEvent){
+
+    }
+
+    public void onClick(javafx.event.ActionEvent actionEvent) {
+        String login = barraLogin.getText();
+        String password = cajaPassword.getText();
+        if (login.equals("santi") && password.equals("1234")) {
+            System.out.println("Todo ha ido bien");
+        }else {
+            errorCuentaErronea.setText("Opsss... No tienes cuenta :( !!");
+        }
+    }
+}
