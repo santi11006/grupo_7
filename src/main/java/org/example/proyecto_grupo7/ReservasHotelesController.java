dc3666236a40676c233addadd004c7cf50b018a1
santi <santiagofuentesespinosa@gmail.com>
2024-05-22 16:36:28 +0200

    interfaz reservas hoteles

--- src/main/java/org/example/proyecto_grupo7/ReservasHotelesController.java
+++ src/main/java/org/example/proyecto_grupo7/ReservasHotelesController.java
@@ -6,19 +6,31 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 public class ReservasHotelesController {
     private Stage stage;
     private Scene scene;
     private Parent root;
     @FXML
-    private Button volverAtras;
+    private Button volverAtras, hacerReserva;
+
     public void volverAtras(ActionEvent actionEvent) throws IOException {
         stage = (Stage) volverAtras.getScene().getWindow();
         root = FXMLLoader.load(getClass().getResource("alojamiento-view.fxml"));
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void hacerReserva(ActionEvent actionEvent) throws IOException {
+        stage = (Stage) hacerReserva.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("hacer_reservas-view.fxml"));
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
