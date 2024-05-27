e8ba3b0ce9559685ed30dce2e7b53c2dfb52f7ca
santi <santiagofuentesespinosa@gmail.com>
2024-05-27 18:54:06 +0200

    documentación hecha 1

--- src/main/java/org/example/proyecto_grupo7/AlojamientoController.java
+++ src/main/java/org/example/proyecto_grupo7/AlojamientoController.java
@@ -6,11 +6,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * Controlador para la vista de gestión de alojamientos.
+ * @author santi
+ * @version 1.0
+ * @since 27/05/2024
+ */
 public class AlojamientoController {
     @FXML
     public Button botonAlojamieto;
@@ -18,12 +23,21 @@ public class AlojamientoController {
     private Scene scene;
     private Parent root;
     @FXML
-    private Button botonSalir,botonHotel,noCuenta,botonVerClientes;
+    private Button botonSalir, botonHotel, noCuenta, botonVerClientes;
 
+    /**
+     * Método para manejar el evento del botón "Salir".
+     * @param actionEvent El evento de acción generado por el botón "Salir".
+     */
     public void botonSalir(ActionEvent actionEvent) {
         System.exit(0);
     }
 
+    /**
+     * Método para manejar el evento del botón "Hotel".
+     * @param actionEvent El evento de acción generado por el botón "Hotel".
+     * @throws IOException Si ocurre un error al cargar la vista de reservas de hoteles.
+     */
     public void botonHotel(ActionEvent actionEvent) throws IOException {
         stage = (Stage) botonHotel.getScene().getWindow();
         root = FXMLLoader.load(getClass().getResource("reservasHoteles-view.fxml"));
@@ -32,6 +46,11 @@ public class AlojamientoController {
         stage.show();
     }
 
+    /**
+     * Método para manejar el evento del botón "Alojamiento".
+     * @param actionEvent El evento de acción generado por el botón "Alojamiento".
+     * @throws IOException Si ocurre un error al cargar la vista de reservas de apartamentos.
+     */
     public void botonAlojamieto(ActionEvent actionEvent) throws IOException {
         stage = (Stage) botonAlojamieto.getScene().getWindow();
         root = FXMLLoader.load(getClass().getResource("reservasApartamentos.fxml"));
@@ -40,12 +59,16 @@ public class AlojamientoController {
         stage.show();
     }
 
+    /**
+     * Método para manejar el evento del botón "No Cuenta".
+     * @param actionEvent El evento de acción generado por el botón "No Cuenta".
+     * @throws IOException Si ocurre un error al cargar la vista de registro de cuenta.
+     */
     public void noCuenta(ActionEvent actionEvent) throws IOException {
         stage = (Stage) noCuenta.getScene().getWindow();
-        root =FXMLLoader.load(getClass().getResource("registro-view.fxml"));
+        root = FXMLLoader.load(getClass().getResource("registro-view.fxml"));
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-
 }
